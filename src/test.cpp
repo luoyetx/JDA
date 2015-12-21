57c774a6806e3f0782e7a27a7c7dd70581cc484d
zhangjie <luoyetx@gmail.com>
2015-12-21 10:19:48 +0800

    add full fddb test

--- src/test.cpp
+++ src/test.cpp
@@ -28,6 +28,7 @@ void fddb() {
   const Config& c = Config::GetInstance();
   JoinCascador joincascador;
   FILE* fd = fopen("../model/jda.model", "rb");
+  JDA_Assert(fd, "Can not open model file");
   joincascador.SerializeFrom(fd);
   // make sure you set these two variables in `config.json` correctly
   joincascador.current_stage_idx = c.current_stage_idx;
@@ -37,37 +38,43 @@ void fddb() {
   JDA_Assert(EXISTS("../data/fddb"), "No fddb data!");
 
   string prefix = "../data/fddb/images/";
-  string fddb = "../data/fddb/FDDB-folds/FDDB-fold-01.txt";
-  string fddb_out = "../data/fddb/result/fold-01-out.txt";
+  char fddb[300];
+  char fddb_out[300];
+  // full test
+  for (int i = 1; i <= 10; i++) {
+    LOG("Testing FDDB-fold-%02d.txt", i);
+    sprintf(fddb, "../data/fddb/FDDB-folds/FDDB-fold-%02d.txt", i);
+    sprintf(fddb_out, "../data/fddb/result/fold-%02d-out.txt", i);
 
-  FILE* fin = fopen(fddb.c_str(), "r");
-  JDA_Assert(fin, "Can not open FDDB-fold-01.txt");
-  FILE* fout = fopen(fddb_out.c_str(), "w");
-  JDA_Assert(fin, "Can not open fold-01-out.txt");
+    FILE* fin = fopen(fddb, "r");
+    JDA_Assert(fin, "Can not open fddb");
+    FILE* fout = fopen(fddb_out, "w");
+    JDA_Assert(fin, "Can not open fddb_out");
 
-  char path[300];
-  while (fscanf(fd, "%s", path) > 0) {
-    string full_path = prefix + string(path) + string(".jpg");
-    Mat img = imread(full_path, CV_LOAD_IMAGE_GRAYSCALE);
-    if (!img.data) {
-      LOG("Can not open %s, Skip it", full_path);
+    char path[300];
+    while (fscanf(fd, "%s", path) > 0) {
+      string full_path = prefix + string(path) + string(".jpg");
+      Mat img = imread(full_path, CV_LOAD_IMAGE_GRAYSCALE);
+      if (!img.data) {
+        LOG("Can not open %s, Skip it", full_path.c_str());
+      }
+      vector<double> scores;
+      vector<Rect> rects;
+      vector<Mat_<double> > shapes;
+      joincascador.Detect(img, rects, scores, shapes);
+
+      const int n = rects.size();
+      fprintf(fout, "%s\n%d\n", path, n);
+      LOG("%s get %d faces", path, n);
+
+      for (int i = 0; i < n; i++) {
+        Rect& r = rects[i];
+        double s = scores[i];
+        fprintf(fout, "%d %d %d %d %lf\n", r.x, r.y, r.width, r.height, s);
+      }
     }
-    vector<double> scores;
-    vector<Rect> rects;
-    vector<Mat_<double> > shapes;
-    joincascador.Detect(img, rects, scores, shapes);
 
-    const int n = rects.size();
-    fprintf(fout, "%s\n%d\n", path, n);
-    LOG("%s get %d faces", path, n);
-
-    for (int i = 0; i < n; i++) {
-      Rect& r = rects[i];
-      double s = scores[i];
-      fprintf(fout, "%d %d %d %d %lf\n", r.x, r.y, r.width, r.height, s);
-    }
+    fclose(fin);
+    fclose(fout);
   }
-
-  fclose(fin);
-  fclose(fout);
 }
