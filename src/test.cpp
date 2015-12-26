7d092f99ebb2a66d8f45e4e3e367f9b5c7ae62d6
zhangjie <luoyetx@gmail.com>
2015-12-26 18:48:30 +0800

    add test

--- src/test.cpp
+++ src/test.cpp
@@ -23,6 +23,66 @@ using namespace cv;
 using namespace std;
 using namespace jda;
 
+/*! \breif Test over your own data */
+void test() {
+  Config& c = Config::GetInstance();
+  c.shift_size = 0; // no shift
+  // set img_o_size, img_h_size and img_q_size by fddb.minimum_size
+  const int size = c.fddb_minimum_size;
+  c.img_o_size = size;
+  c.img_h_size = int(std::sqrt(size*size / 2.));
+  c.img_q_size = int(std::sqrt(size*size / 4.));
+
+  JoinCascador joincascador;
+  FILE* fd = fopen("../model/jda.model", "rb");
+  JDA_Assert(fd, "Can not open model file");
+  joincascador.SerializeFrom(fd);
+  fclose(fd);
+
+  JDA_Assert(EXISTS("../data/test.txt"), "No test.txt!");
+  if (!EXISTS("../data/test_result")) {
+    MKDIR("../data/test_result");
+  }
+
+  FILE* fin = fopen("../data/test.txt", "r");
+  char path[300];
+  int counter = 0;
+  while (fscanf(fin, "%[^\n]\n", path) > 0) {
+    Mat img = imread(path);
+    if (!img.data) {
+      LOG("Can not open %s, Skip it", path);
+      continue;
+    }
+    Mat gray;
+    cvtColor(img, gray, CV_BGR2GRAY);
+    vector<double> scores;
+    vector<Rect> rects;
+    vector<Mat_<double> > shapes;
+    joincascador.Detect(gray, rects, scores, shapes);
+
+    const int n = rects.size();
+    LOG("%s get %d faces", path, n);
+
+    for (int j = 0; j < n; j++) {
+      const Rect& r = rects[j];
+      double score = scores[j];
+      const Mat_<double> shape = shapes[j];
+      cv::rectangle(img, r, Scalar(0, 0, 255), 3);
+      for (int k = 0; k < c.landmark_n; k++) {
+        cv::circle(img, Point(shape(0, 2 * k), shape(0, 2 * k + 1)), 3, Scalar(0, 255, 0), -1);
+      }
+    }
+    char buff[300];
+    if (c.fddb_result) {
+      counter++;
+      sprintf(buff, "../data/test_result/%04d.jpg", counter);
+      cv::imwrite(buff, img);
+    }
+  }
+
+  fclose(fin);
+}
+
 /*!
  * \breif Test JoinCascador Face Detection over FDDB
  */
@@ -75,6 +135,7 @@ void fddb() {
       Mat img = imread(full_path);
       if (!img.data) {
         LOG("Can not open %s, Skip it", full_path.c_str());
+        continue;
       }
       Mat gray;
       cvtColor(img, gray, CV_BGR2GRAY);
